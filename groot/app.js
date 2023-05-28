f495f4f8199df8fb1236cf0cc4994d89054bb470
sahilborse18 <sahilborse.909098@gmail.com>
2023-05-28 23:37:20 +0530

    html docs

---
+++ groot/app.js
@@ -0,0 +1,50 @@
+let input=document.querySelector("#INPUT")
+let output=document.querySelector("#OUTPUT")
+let TRAN_btn=document.queryselector("#btn-tran")
+
+function clickHandler(){
+    intext=input.value
+    console.log(intext)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
