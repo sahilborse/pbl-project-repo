a2eb598ed726ddc3bf6c2459d5641abb84e26775
sahilborse18 <sahilborse.909098@gmail.com>
2023-05-30 20:09:57 +0530

    new

--- groot/app.js
+++ groot/app.js
@@ -2,10 +2,29 @@ let input=document.querySelector("#INPUT")
 let output=document.querySelector("#OUTPUT")
 let TRAN_btn=document.queryselector("#btn-tran")
 
+let url = "https://api.funtranslations.com/translate/groot.json"
+function transLate(input){
+    return url + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("An error has occured please try again later");
+    alert("error has occured try again after sometime")
+}
+
 function clickHandler(){
     intext=input.value
     console.log(intext)
+    fetch(transLate(intext))
+        .then(response=>response.json())
+        .then(json => {
+            let newText =json.content.translated;
+             output.innerText = newText;
+            })
+        .catch(errorHandler)
+
 }
+TRAN_btn.addEventListner("click", clickHandler);
 
 
 
